dd5ca64fc34f81996162681d951ed654b57d9958
eunjae <pcn04252@naver.com>
2026-07-01 17:02:08 +0900

    test(similarity): add similarity integration cases

--- test_similarity_checker.py
+++ test_similarity_checker.py
@@ -5,3 +5,14 @@ def test_same_string_returns_100():
     checker = SimilarityChecker()
 
     assert checker.score("ABCDE", "ABCDE") == 100
+
+
+def test_completely_different_returns_0():
+    checker = SimilarityChecker()
+
+    assert checker.score("A", "BB") == 0
+
+def test_partial_similarity():
+    checker = SimilarityChecker()
+
+    assert checker.score("AA", "AAE") == 50
